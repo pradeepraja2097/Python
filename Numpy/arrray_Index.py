2a8c1bd2dac96b8f5a78639395d0481a9935cca8
Pradeep_raja <pradeepraj2097@gmail.com>
2021-04-24 22:30:30 +0530

    update

---
+++ Numpy/arrray_Index.py
@@ -0,0 +1,14 @@
+import numpy as np
+array_index=([1,2,3,4])# it is an single dimensional array
+print(array_index[0])# it prints the first value
+
+
+
+array_2dimensional_index=np.array([[1,3,4],[5,6,7]])#its a two dimensional array
+print(array_2dimensional_index.ndim)#it prints a dimensional value
+print(array_2dimensional_index[0,1])#it prints a first list 2nd element
+print(array_2dimensional_index[1,1])#it prints a second list 2nd element
+
+
+
+
---
+++ Numpy/dimensional_arrays.py
@@ -0,0 +1,8 @@
+import numpy as np
+array_twodimensional=np.array([[[1,2,4],[5,6,7]]])
+print(array_twodimensional)
+print(array_twodimensional.ndim)
+
+
+
+
