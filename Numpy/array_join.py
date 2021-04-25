88351665c88c84f14669c3cf6f13cf3852b92c40
Pradeep_raja <pradeepraj2097@gmail.com>
2021-04-25 07:42:53 +0530

    array concatenate

---
+++ Numpy/array_join.py
@@ -0,0 +1,7 @@
+import numpy as np
+array_1=np.array([1,2,3])  # furst array
+array_2=np.array([4,5,6])  #Second array
+
+array_concatenate=np.concatenate((array_1,array_2))  #By using Concatenate Function
+
+print(array_concatenate) # It prints the concatenate value of array
