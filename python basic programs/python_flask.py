34a60d0e5876c4ad7c8adf476bd2a3e79d6474d3
Pradeep_raja <pradeepraj2097@gmail.com>
2021-07-21 11:14:48 +0530

    html css

---
+++ python basic programs/python_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/') #@app.route('www.domainname.com/') your domain name (simple "/ " represents local host)
+ 
+def hello():
+    return "Pradeep"
+
+if __name__ =='__main__':
+    app.run(debug=True)
